9550276edf0e6314cf9d988476087c995cfee2a6
nahlandyza <nahla.damayanti@gmail.com>
2025-06-24 21:18:02 +0700

    add inventory schema

---
+++ apps/api/src/controllers/product-controller.ts
@@ -0,0 +1,9 @@
+export async function createProduct() {}
+
+export async function getAllProduct() {}
+
+export async function getProductById() {}
+
+export async function updateProduct() {}
+
+export async function deleteProduct() {}
